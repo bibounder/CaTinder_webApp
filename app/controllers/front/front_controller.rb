b547241d348853c37e726b4f441c9be925760ae1
vendethiel <vendethiel@hotmail.fr>
2015-04-26 19:46:21 +0200

    dasdsdsa

--- app/controllers/front/front_controller.rb
+++ app/controllers/front/front_controller.rb
@@ -1,4 +1,8 @@
 class Front::FrontController < ::ApplicationController
+  # don't protect from forgery in the API
+  protect_from_forgery with: :exception
+
+
   before_action :authenticate!
 
   helper_method :anonymous?, :authenticated?
